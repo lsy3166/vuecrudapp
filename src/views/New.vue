e9bc0094bcead6d46acbb9de13fa7b51d1801d76
LSY3166\user <lsy3166@gmail.com>
2021-03-23 18:08:09 +0900

    init task with mongo db

---
+++ src/views/New.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>New task</h1>
+    <!-- <task-form @createOrUpdate="createOrUpdate"></task-form> -->
+    <task-form @createOrUpdate="createOrUpdate"></task-form>
+  </div>
+</template>
+
+<script>
+import TaskForm from "../components/Taskform.vue";
+import { api } from "../helpers/helpers";
+export default {
+  name: "new-task",
+  components: { TaskForm },
+  methods: {
+    createOrUpdate: async function(task) {
+      const res = await api.createtask(task);
+      this.flash("task created", "success");
+      this.$router.push(`/tasks/${res._id}`);
+    }
+  }
+};
+</script>
